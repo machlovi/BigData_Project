75d4ce62422a441dc9e58cf1eaf9d210c50a3947
Naseem Machlovi <naseem.machlovi@gmail.com>
2022-04-06 05:21:46 -0400

    gg

--- part1/Part2/mapper.py
+++ part1/Part2/mapper.py
@@ -4,16 +4,18 @@
 
 import sys
 import re
-rule = re.compile('[^\s]{4}[A|P]')
+rule = re.compile('[A-Z]\s[^\s]{3}')
 
 for line in sys.stdin:
-  line=line.strip(',').split(',')
-  line_len = len(line)
-  if line_len ==43:
-    vtime = line[19]
-    match = rule.search(vtime)
-    if match:
-      vtime=vtime[:2]+vtime[-1:]# Only printing the Violation time
-      print('%s\t%s' % (vtime, '1'))
-  else:
-      continue
+    line=line.strip(',').split(',')
+    line_len = len(line)
+    if line_len ==43:
+
+      car_type= line[6]
+      year= line[35]
+      car_descrp=car_type+' '+year
+      match = rule.search(car_descrp)
+      if match:
+        print('%s\t%s' % (car_descrp, '1'))
+      else:
+        continue
